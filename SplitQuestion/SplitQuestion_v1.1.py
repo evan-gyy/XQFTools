ade4375113ae4004801cdcc304af2fa0f76b44a5
evan-gyy <2420298062@qq.com>
2022-02-18 18:50:54 +0800

    Update: SplitQuestion

--- SplitQuestion/SplitQuestion_v1.1.py
+++ SplitQuestion/SplitQuestion_v1.1.py
@@ -122,18 +122,17 @@ class SplitQuestion:
         self.path = path
         self.excel = self.find_file('.xlsx', path)
         self.word_list = self.find_file('.docx', path, multiple=True)
-        self.word_list.sort(key=lambda x: len(x))
+        # self.word_list.sort(key=lambda x: int(x.split('-')[0]))
         self.new_ques_num = {}
 
     def find_file(self, type, path, multiple=False):
         file_list = []
-        for root, dirs, files in os.walk(path):
-            for f in files:
-                if type in f:
-                    if not multiple:
-                        return f
-                    else:
-                        file_list.append(f)
+        for f in os.listdir(path):
+            if type in f and 'res' not in f and 'name-mode' not in f:
+                if not multiple:
+                    return f
+                else:
+                    file_list.append(f)
         return file_list
 
     def get_ques_num(self):
@@ -144,11 +143,13 @@ class SplitQuestion:
         for i in range(0, len(cat_df)):
             if isinstance(cat_df.iloc[i]['章'], str):
                 count += 1
+                cat_df.loc[i, '章'] = str(count) + "、" + cat_df.loc[i, '章']
                 ques_num_dict[count] = []
                 titles.append(cat_df.iloc[i]['章'])
             num = cat_df.iloc[i]['习题编号']
             if isinstance(num, str):
                 ques_num_dict[count].append(num)
+        cat_df.to_excel(self.path + 'res-' + self.excel, index=False)
         return ques_num_dict, titles
 
     def rename_number(self, old_ques_num):
@@ -160,6 +161,9 @@ class SplitQuestion:
         return ques_dict
 
     def to_file(self, ques_dict, each=True, total=True):
+        for f in os.listdir(self.path):
+            if f.endswith('.txt'):
+                os.remove(os.path.join(self.path, f))
         if each:
             for key, value in ques_dict.items():
                 name = self.path + self.word_list[key - 1][:-5] + '_' + str(len(value)) + '.txt'
@@ -182,34 +186,82 @@ class SplitQuestion:
                 os.remove(new_name)
             os.rename(name, new_name)
 
-    def word_rename(self, path, titles, mode):
+    def word_rename(self, path, titles, default_mode=1, imp=False):
+        if not imp:
+            order_list = [i + 1 for i in range(len(titles))]
+            mode_list = [default_mode for j in range(len(titles))]
+            self.generate_mode_file(order_list, mode_list)
+            input('请核对编号-模式文件')
+
+        rename_file = ""
+        for f in os.listdir(path):
+            if 'name-mode' in f:
+                rename_file = f
+        if rename_file == "":
+            raise ValueError("无法找到编号-模式文件，请检查目录")
+        rn_df = pd.read_excel(path + rename_file)
+        if rn_df.shape[0] != len(titles):
+            raise ValueError("编号-模式数量与文件实际数量不符，请核对")
         for t in range(len(titles)):
-            match = difflib.get_close_matches(titles[t], self.word_list, 1, cutoff=0.7)[0]
-            os.rename(path + match, path + f"{t + 1}-{str(mode)}.docx")
+            try:
+                match = difflib.get_close_matches(titles[t], self.word_list, 1, cutoff=0.7)[0]
+            except:
+                match = 0
+            if match:
+                os.rename(path + match, path + f"{rn_df.loc[t, 'order']}-{rn_df.loc[t, 'mode']}.docx")
+            else:
+                for f in self.word_list:
+                    if f.split('-')[0] == str(t + 1):
+                        os.rename(path + f, path + f"{rn_df.loc[t, 'order']}-{rn_df.loc[t, 'mode']}.docx")
+
+
+    def generate_mode_file(self, order: list , mode: list):
+        res_df = pd.DataFrame()
+        res_df['order'] = order
+        res_df['mode'] = mode
+        for f in os.listdir(self.path):
+            if 'name-mode' in f:
+                ans = input('已有编号-模式文件，是否替换？(y/n)：')
+                if ans in ['y', '']:
+                    res_df.to_excel(self.path + 'name-mode-' + self.excel, index=False)
+                    print('已生成编号-模式文件')
+                else:
+                    pass
+                return 0
+        res_df.to_excel(self.path + 'name-mode-' + self.excel, index=False)
+        print('已生成编号-模式文件')
 
     def run(self):
         # Step 1
         self.old_ques_num, titles = self.get_ques_num()
         rename = False
+        imp = False
         while True:
             ans = input("是否需要重命名？（y/n）：")
-            if ans == "y":
+            if ans in ['y', '']:
                 rename = True
+                if input("是否导入编号-模式列表？（y/n）：") in ['y', '']:
+                    imp = True
                 break
             elif ans == "n":
                 break
             else:
                 print("请输入y或n")
-        if rename:
+        if rename and not imp:
             mode = int(input("请输入默认模式："))
             self.word_rename(self.path, titles, mode)
-            input("请核对文件名及模式，按回车继续")
+        elif imp:
+            self.word_rename(self.path, titles, imp=True)
+        input("请核对文件名及模式，按回车继续")
         # Step 2
         self.word_list = self.find_file('.docx', self.path, multiple=True)
-        self.word_list.sort(key=lambda x: len(x))
+        self.word_list.sort(key=lambda x: int(x.split('-')[0]))
+        order_list = [x.split('-')[0] for x in self.word_list]
         print('文件列表：', self.word_list)
+
         self.mode_list = [int(i[:-5].split("-")[1]) for i in self.word_list]
         print('模式列表：', self.mode_list)
+
         # Step 3
         self.new_ques_num = self.rename_number(self.old_ques_num)
         self.to_file(self.new_ques_num)
--- SplitQuestion/exe/SplitQuestion_v1.1.spec
+++ SplitQuestion/exe/SplitQuestion_v1.1.spec
@@ -1,16 +1,14 @@
 # -*- mode: python ; coding: utf-8 -*-
 
-
 block_cipher = None
 
 
 a = Analysis(['..\\SplitQuestion_v1.1.py'],
-             pathex=[],
+             pathex=['D:\\Git\\xqftools\\SplitQuestion\\exe'],
              binaries=[],
              datas=[],
              hiddenimports=[],
              hookspath=[],
-             hooksconfig={},
              runtime_hooks=[],
              excludes=[],
              win_no_prefer_redirects=False,
@@ -19,12 +17,11 @@ a = Analysis(['..\\SplitQuestion_v1.1.py'],
              noarchive=False)
 pyz = PYZ(a.pure, a.zipped_data,
              cipher=block_cipher)
-
 exe = EXE(pyz,
           a.scripts,
           a.binaries,
           a.zipfiles,
-          a.datas,  
+          a.datas,
           [],
           name='SplitQuestion_v1.1',
           debug=False,
@@ -33,8 +30,4 @@ exe = EXE(pyz,
           upx=True,
           upx_exclude=[],
           runtime_tmpdir=None,
-          console=True,
-          disable_windowed_traceback=False,
-          target_arch=None,
-          codesign_identity=None,
-          entitlements_file=None )
+          console=True )
